0db153ec35a44ac7cbbf1aebac8b294d8b5f506f
Jigar Gosar <jigar.gosar@gmail.com>
2016-07-27 16:39:56 +0530

    fix invalid call to function lensValueEquals in mergeLensValuesWhenEqual

--- app/js/model.js
+++ app/js/model.js
@@ -26,15 +26,13 @@ TFP.APP2048.Grid = (function (_, A) {
     var mergeLensValuesWhenEqual = function (lens1, lens2) {
         var lensValueEquals = _.converge(_.equals, [_.view(lens1), _.view(lens2)]);
         var mergeLensValues = _.compose(_.set(lens2, 0), _.over(lens1, _.multiply(2)));
-        return _.when(lensValueEquals(lens1, lens2), mergeLensValues);
+        return _.when(lensValueEquals, mergeLensValues);
     };
 
     var addAdjacentNums = function (list) {
-
         var lensPairs = _.compose(_.aperture(2), _.times(_.lensIndex), _.length)(list);
 
         var updateListReducer = function (list, lensPair) {
-            debugger;
             return _.apply(mergeLensValuesWhenEqual, lensPair)(list);
         };
         return _.reduce(updateListReducer, list, lensPairs);
@@ -46,14 +44,11 @@ TFP.APP2048.Grid = (function (_, A) {
         createRandomGrid: createGrid,
         mapNum: mapNum,
         toList: mapNum(_.identity),
-        slideLeft: function (grid) {
-            return _.compose(
-                _.unnest,
-                compressRows,
-                _.map(addAdjacentNums),
-                compressRows,
-                _.splitEvery(4))
-            (grid);
-        }
+        slideLeft: _.compose(
+            _.unnest,
+            compressRows,
+            _.map(addAdjacentNums),
+            compressRows,
+            _.splitEvery(4))
     };
 })(TFP.Functional, G.Assert);
