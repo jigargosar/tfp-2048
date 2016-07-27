d4276c7b8f7689e7b47e92bac7aeae5ae94d16f3
Jigar Gosar <jigar.gosar@gmail.com>
2016-07-27 19:13:20 +0530

    implemented slideRight and slideBottom

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,7 @@
 
 TFP.APP2048.Main = (function (_, mount, Grid, RootView) {
 
-    var grid = Grid();
+    var grid = Grid.createRandomGrid();
     console.log("Grid created: ", grid);
     mount("root", RootView(grid));
 })(TFP.Functional, G.DOM.mount, TFP.APP2048.Grid, TFP.APP2048.RootView);
