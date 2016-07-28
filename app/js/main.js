ace81dda4fb0de24ca0413b1dd61e2bcbb5d7a5f
Jigar Gosar <jigar.gosar@gmail.com>
2016-07-28 15:43:56 +0530

    simplify keyCode to slidGridFnMapping

--- app/js/main.js
+++ app/js/main.js
@@ -11,21 +11,23 @@ TFP.APP2048.Main = (function (_, mount, G, RootView) {
 
     render();
 
-    const ArrowUp = 38;
-    const ArrowDown = 40;
-    const ArrowLeft = 37;
-    const ArrowRight = 39;
-    window.addEventListener("keyup", function (event) {
-        console.log(event.key, event.keyCode);
-        var gridFn = _.cond([
-            [_.equals(ArrowUp), _.always(G.slideUp)],
-            [_.equals(ArrowDown), _.always(G.slideDown)],
-            [_.equals(ArrowLeft), _.always(G.slideLeft)],
-            [_.equals(ArrowRight), _.always(G.slideRight)],
-            [_.T, _.identity]
-        ])(event.keyCode);
+    var slideGridForKeyCode = function (keyCode, grid) {
+        const ArrowUp = 38;
+        const ArrowDown = 40;
+        const ArrowLeft = 37;
+        const ArrowRight = 39;
 
-        grid = gridFn(grid);
+        var keyCodes = [ArrowUp, ArrowDown, ArrowLeft, ArrowRight];
+        var slideFns = [G.slideUp, G.slideDown, G.slideLeft, G.slideRight];
+        var mapping = _.zipObj(keyCodes, slideFns);
+
+        var safeSlide = _.propOr(_.identity, keyCode, mapping);
+        return safeSlide(grid);
+    };
+
+    window.addEventListener("keyup", function (event) {
+        // console.log(event.key, event.keyCode);
+        grid = slideGridForKeyCode(event.keyCode, grid);
         render();
     });
 })(TFP.Functional, G.DOM.mount, TFP.APP2048.Grid, TFP.APP2048.RootView);
